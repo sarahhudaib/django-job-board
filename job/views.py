a7f38bbccaf9d13122fdf8d08bbfbcdb15f84f29
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-12-21 14:02:03 +0300

    Added Job views & templates

--- job/views.py
+++ job/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+# Model Queryset in Django
+# https://docs.djangoproject.com/en/4.1/ref/models/querysets/
+
+# Will Retrieve all jobs 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    context = {'jobs': job_list} # template name
+    return render(request, 'job/job_list.html', context)
 
+# Will Retrieve one jobs details
 def job_detail(request, id):
-    pass
+    job_detail = Job.objects.get(id=id) # will retrieve on job
+    # job_detail = Job.object.filter() # will retrieve on job from a list according to some filtration
+    context = {'job': job_detail}
+    return render(request,'job/job_detail.html', context)
