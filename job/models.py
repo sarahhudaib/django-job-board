6e766cb1f83c06ec049996ea2b3e12b66c5bbbdb
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-12-21 12:35:27 +0300

    Added Job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+'''
+ django model field : 
+    - html widget
+    - validation 
+    - db size 
+'''
+
+JOB_TYPE = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
+#https://docs.djangoproject.com/en/4.1/ref/models/fields/
+class Job(models.Model):  
+    title = models.CharField(max_length=100)  
+    job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1) 
+    
+    def __str__(self):
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My Apps
+    'accounts',
+    'blog',
+    'contact',
+    'home',
+    'job',
 ]
 
 MIDDLEWARE = [
