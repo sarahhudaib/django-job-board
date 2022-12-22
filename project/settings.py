4002292ca6a0ea766bab281bd1f8b36b96e36096
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-12-22 10:31:40 +0300

    Added Job details static files and connect them to the base.html

--- project/settings.py
+++ project/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
