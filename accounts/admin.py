85647139b8bbb880086ff8b0c1f1fa58b6166671
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2023-01-07 12:10:37 +0300

    Added the accounts profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile , City
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'phone_number', 'image']
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(City, CityAdmin)
